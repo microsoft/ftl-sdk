f5300789ca77860c4ab8c384f5117d33ad050ac6
Stefan Slivinski <sslivins@gmail.com>
2016-10-24 22:04:05 -0700

    added jansson library

--- ftl_app/main.c
+++ ftl_app/main.c
@@ -33,6 +33,7 @@
 #endif
 #include "file_parser.h"
 #include <curl/curl.h>
+#include <jansson.h>
 
 void sleep_ms(int ms)
 {
@@ -49,6 +50,19 @@ void log_test(ftl_log_severity_t log_level, const char * message) {
 }
 
 char *_get_ingests(char *host);
+/* forward refs */
+void print_json(json_t *root);
+void print_json_aux(json_t *element, int indent);
+void print_json_indent(int indent);
+const char *json_plural(int count);
+void print_json_object(json_t *element, int indent);
+void print_json_array(json_t *element, int indent);
+void print_json_string(json_t *element, int indent);
+void print_json_integer(json_t *element, int indent);
+void print_json_real(json_t *element, int indent);
+void print_json_true(json_t *element, int indent);
+void print_json_false(json_t *element, int indent);
+void print_json_null(json_t *element, int indent);
 
 void usage() {
     printf("Usage: ftl_app -i <ingest uri> -s <stream_key> - v <h264_annex_b_file> -a <opus in ogg container>\n");
@@ -127,7 +141,13 @@ int main(int argc, char** argv) {
 		usage();
 	}	
 
-	_get_ingests(ingest_location);
+	char *ingest_list = _get_ingests(ingest_location);
+	json_t *ingests;
+	json_error_t error;
+
+	ingests = json_loads(ingest_list, 0, &error);
+
+	print_json(ingests);
 
 	FILE *video_fp = NULL;	
 	uint32_t len = 0;
@@ -421,7 +441,7 @@ cleanup:
 		}
 
 		strncpy(query_result, chunk.memory, chunk.size);
-		printf("query returned: %s\n", query_result);
+		query_result[chunk.size] = '\0';
 	}
 	else {
 		printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
@@ -433,4 +453,108 @@ cleanup:
 	 curl_easy_cleanup(curl_handle);
 
 	 return query_result;
+ }
+
+ void print_json(json_t *root) {
+	 print_json_aux(root, 0);
+ }
+
+ void print_json_aux(json_t *element, int indent) {
+	 switch (json_typeof(element)) {
+	 case JSON_OBJECT:
+		 print_json_object(element, indent);
+		 break;
+	 case JSON_ARRAY:
+		 print_json_array(element, indent);
+		 break;
+	 case JSON_STRING:
+		 print_json_string(element, indent);
+		 break;
+	 case JSON_INTEGER:
+		 print_json_integer(element, indent);
+		 break;
+	 case JSON_REAL:
+		 print_json_real(element, indent);
+		 break;
+	 case JSON_TRUE:
+		 print_json_true(element, indent);
+		 break;
+	 case JSON_FALSE:
+		 print_json_false(element, indent);
+		 break;
+	 case JSON_NULL:
+		 print_json_null(element, indent);
+		 break;
+	 default:
+		 fprintf(stderr, "unrecognized JSON type %d\n", json_typeof(element));
+	 }
+ }
+
+ void print_json_indent(int indent) {
+	 int i;
+	 for (i = 0; i < indent; i++) { putchar(' '); }
+ }
+
+ const char *json_plural(int count) {
+	 return count == 1 ? "" : "s";
+ }
+
+ void print_json_object(json_t *element, int indent) {
+	 size_t size;
+	 const char *key;
+	 json_t *value;
+
+	 print_json_indent(indent);
+	 size = json_object_size(element);
+
+	 printf("JSON Object of %ld pair%s:\n", size, json_plural(size));
+	 json_object_foreach(element, key, value) {
+		 print_json_indent(indent + 2);
+		 printf("JSON Key: \"%s\"\n", key);
+		 print_json_aux(value, indent + 2);
+	 }
+ }
+
+ void print_json_array(json_t *element, int indent) {
+	 size_t i;
+	 size_t size = json_array_size(element);
+	 print_json_indent(indent);
+
+	 printf("JSON Array of %ld element%s:\n", size, json_plural(size));
+	 for (i = 0; i < size; i++) {
+		 print_json_aux(json_array_get(element, i), indent + 2);
+	 }
+ }
+
+ void print_json_string(json_t *element, int indent) {
+	 print_json_indent(indent);
+	 printf("JSON String: \"%s\"\n", json_string_value(element));
+ }
+
+ void print_json_integer(json_t *element, int indent) {
+	 print_json_indent(indent);
+	 printf("JSON Integer: \"%" JSON_INTEGER_FORMAT "\"\n", json_integer_value(element));
+ }
+
+ void print_json_real(json_t *element, int indent) {
+	 print_json_indent(indent);
+	 printf("JSON Real: %f\n", json_real_value(element));
+ }
+
+ void print_json_true(json_t *element, int indent) {
+	 (void)element;
+	 print_json_indent(indent);
+	 printf("JSON True\n");
+ }
+
+ void print_json_false(json_t *element, int indent) {
+	 (void)element;
+	 print_json_indent(indent);
+	 printf("JSON False\n");
+ }
+
+ void print_json_null(json_t *element, int indent) {
+	 (void)element;
+	 print_json_indent(indent);
+	 printf("JSON Null\n");
  }

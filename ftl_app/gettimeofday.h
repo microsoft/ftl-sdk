dac6cb845adf702201c5566ad6c3944b8ae3224a
Stefan Slivinski <sslivins@gmail.com>
2017-05-23 06:52:41 -0700

    fixed delclaration

--- ftl_app/gettimeofday.h
+++ ftl_app/gettimeofday.h
@@ -7,7 +7,7 @@ BOOLEAN nanosleep(LONGLONG ns);
 #else
 #include <sys/time.h>
 #endif
-int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y);
+int timeval_subtract(struct timeval *result, const struct timeval *end, const struct timeval *start);
 float timeval_to_ms(struct timeval *tv);
 uint64_t timeval_to_us(struct timeval *tv);
 int64_t timeval_subtract_to_ms(const struct timeval *end, const struct timeval *start);
